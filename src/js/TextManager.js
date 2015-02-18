d156f8631eb2d0719056ee7c7ce2be824ef42775
lmatiolis <matioli.leandro@gmail.com>
2015-02-18 20:26:05 -0200

    Change TextManager to prototype pattern.

--- src/js/TextManager.js
+++ src/js/TextManager.js
@@ -1,57 +1,55 @@
 nsn.TextManager = function(){
 
-  var self = {};
+  this._font = "35px Mouse Memoirs";
 
-  self.textObject = null;
-  var canvasContext = null;
-  var timeout = 7000;
+  this._textObject = this._createTextObject();
 
-  var nextLineOfText = "";
+  this._isShowingDialog = false;
 
-  var currentDeferred;
+  this._nextLineOfText = "";
 
-  self.isShowingDialog = false;
+  this.canvasContext = Engine.canvas.getContext("2d");
 
-  function init(){
+  this._defaultTimeout = 7000;
 
-    setSkipTextOnKeypress();
+  this._textLayer = new createjs.Container();
 
-    var font = "35px Mouse Memoirs";
-    self.textObject = new createjs.Text("", font, "#FFFFFF");
-    self.textObject.x = 100;
-    self.textObject.y = 545;
-    self.textObject.textBaseline = "alphabetic";
-    self.textObject.lineHeight = 30;
-    self.textObject.shadow = new createjs.Shadow("#000000", 2, 2, 0);
+  this.textContainer = new createjs.Container();
 
-    self.textLayer = new createjs.Container();
+  this.currentDeferred;
+
+  this.init();
+
+};
+
+nsn.TextManager.prototype = {
+
+  init: function(){
+
+    this._setSkipTextOnKeypress();
 
     var graphics = new createjs.Graphics();
     var shape = new createjs.Shape(graphics);
     graphics.beginFill("rgba(255,255,255,0.01)");
-    // graphics.beginFill("rgba(0,0,255,0.5)");
     graphics.drawRect(0, 0, Engine.canvas.width, Engine.canvas.height);
 
-    self.textLayer.addChild(shape);
+    this._textLayer.addChild(shape);
 
-    self.textLayer.alpha = 1;
+    this._textLayer.alpha = 1;
 
-    self.textContainer = new createjs.Container();
-    self.textContainer.addChild(self.textLayer);
-    self.textContainer.addChild(self.textObject);
+    this.textContainer.addChild(this._textLayer);
+    this.textContainer.addChild(this._textObject);
 
-    canvasContext = Engine.canvas.getContext("2d");
-    canvasContext.font = font;
+    this.canvasContext.font = this._font;
 
-    self.textLayer.addEventListener('click', self.clearText);
+    this._textLayer.addEventListener('click', this.clearText);
 
-    nsn.listen(nsn.events.SCENE_CHANGED, self.hideText);
-  }
+    nsn.listen(nsn.events.SCENE_CHANGED, this.hideText);
+  },
 
-  function setSkipTextOnKeypress(){
+  _setSkipTextOnKeypress: function (){
     $(document).keypress(function(event){
       var keyCode = (event.keyCode ? event.keyCode : event.which);
-      console.log(keyCode);
 
       /* KeyCodes
        *
@@ -60,23 +58,39 @@ nsn.TextManager = function(){
        *
        */
       if (keyCode == 32 || keyCode == 46){
-        // self.hideText();
-        self.clearText();
+        this.clearText();
       }
     });
-  }
+  },
 
-  self.showText = function(text, customTimeout){
-    var textBrokenInLines = splitTextInLines(text);
-    var textWithLineBreaks = "";
-    var textTimeout = customTimeout || timeout;
+  clearText: function(){
 
-    if(currentDeferred && currentDeferred.promise().state() != "resolved"){
-      currentDeferred.resolve();
+    this.hideText();
+
+    if(this._nextLineOfText === ""){
+      nsn.fire(nsn.events.TEXT_END);
+      this.currentDeferred.resolve();
     }
-    currentDeferred = new $.Deferred();
 
-    self.stopAllTexts();
+  },
+
+  hideText: function(){
+    this._textObject.text = "";
+    this._isShowingDialog = false;
+    this._textLayer.alpha = 0;
+  },
+
+  showText: function(text, customTimeout){
+    var textBrokenInLines = this._splitTextInLines(text);
+    var textWithLineBreaks = "";
+    var textTimeout = customTimeout || _defaultTimeout;
+
+    if(this.currentDeferred && this.currentDeferred.promise().state() != "resolved"){
+      this.currentDeferred.resolve();
+    }
+    this.currentDeferred = new $.Deferred();
+
+    this.stopAllTexts();
 
     if(textBrokenInLines.length > 2){
 
@@ -84,23 +98,23 @@ nsn.TextManager = function(){
 
       //Obs: Esses loops devem pesar, não? Tem um igual no cara andando...
       (function loopToShowBigTexts () {
-         self.showTextTimeout = setTimeout(function () {
+         this.showTextTimeout = setTimeout(function () {
 
-          if(!self.isShowingText()){
+          if(!this.isShowingText()){
 
-            textWithLineBreaks = getTwoLinesOfText(textBrokenInLines, linesCounter);
+            textWithLineBreaks = this._getTwoLinesOfText(textBrokenInLines, linesCounter);
 
-            nextLineOfText = textWithLineBreaks;
+            this._nextLineOfText = textWithLineBreaks;
 
-            self.isShowingDialog = true;
+            this._isShowingDialog = true;
 
-            renderText(textWithLineBreaks, textTimeout);
+            this._renderText(textWithLineBreaks, textTimeout);
             linesCounter += 2;
 
             if (linesCounter < textBrokenInLines.length){
               loopToShowBigTexts();
             }else{
-              nextLineOfText = "";
+              this._nextLineOfText = "";
             }
 
           }else{
@@ -108,27 +122,48 @@ nsn.TextManager = function(){
           }
 
          }, 100);
-      })();
+      })(this);
 
     }else{
-      nextLineOfText = "";
-      textWithLineBreaks = getTwoLinesOfText(textBrokenInLines, 0);
-      self.isShowingDialog = true;
-      renderText(textWithLineBreaks, textTimeout);
+      this._nextLineOfText = "";
+      textWithLineBreaks = this._getTwoLinesOfText(textBrokenInLines, 0);
+      this._isShowingDialog = true;
+      this._renderText(textWithLineBreaks, textTimeout);
     }
 
-    return currentDeferred.promise();
+    return this.currentDeferred.promise();
 
-  };
+  },
 
-  self.showTextWithoutTimeout = function(text){
-    self.hideText();
-    // self.clearText();
-    renderText(text, 0);
-    self.isShowingDialog = false;
-  };
+  showTextWithoutTimeout: function(text){
+    this.hideText();
+    this._renderText(text, 0);
+    this._isShowingDialog = false;
+  },
 
-  var getTwoLinesOfText = function(textBrokenInLines, indexToStart){
+  _renderText: function(text, timeout){
+    if(this._isShowingDialog){
+      this._textLayer.alpha = 1;
+    }
+    this._textObject.text = text;
+    this._isShowingDialog = true;
+
+    if(textTimeoutId){
+      clearTimeout(textTimeoutId);
+    }
+
+    if(timeout && (timeout > 0)){
+      var textTimeoutId = setTimeout(this.clearText, timeout);
+    }
+
+  },
+
+  stopAllTexts: function() {
+    clearTimeout(this.showTextTimeout);
+    this.hideText();
+  },
+
+  _getTwoLinesOfText: function(textBrokenInLines, indexToStart){
     var textWithLineBreaks = "";
 
     var twoLinesCounter = 0;
@@ -147,59 +182,19 @@ nsn.TextManager = function(){
     }
 
     return textWithLineBreaks;
-  };
+  },
 
-  var renderText = function(text, timeout){
-    if(self.isShowingDialog){
-      self.textLayer.alpha = 1;
-    }
-    self.textObject.text = text;
-    self.isShowingDialog = true;
-
-    if(self.textTimeoutId){
-      clearTimeout(self.textTimeoutId);
-    }
-    if(timeout && (timeout > 0)){
-      // self.textTimeoutId = setTimeout(self.hideText.bind(this), timeout);
-      self.textTimeoutId = setTimeout(self.clearText, timeout);
-    }
-
-  };
-
-  self.clearText = function(){
-
-    self.hideText();
-
-    if(nextLineOfText === ""){
-      console.log("Fim do texto.");
-      nsn.fire(nsn.events.TEXT_END);
-      currentDeferred.resolve();
-    }
-
-  };
-
-  self.hideText = function(){
-    self.textObject.text = "";
-    self.isShowingDialog = false;
-    self.textLayer.alpha = 0;
-  };
-
-  self.isShowingText = function(){
-    if(self.textObject.text === ""){
+  isShowingText: function(){
+    if(this._textObject.text === ""){
       return false;
     }else{
       return true;
     }
-  };
+  },
 
-  self.stopAllTexts = function() {
-    clearTimeout(self.showTextTimeout);
-    self.hideText();
-  };
-
-  var splitTextInLines = function(text){
+  _splitTextInLines: function(text){
     var canvasWidth = Engine.canvas.width;
-    var marginSpace = 2*self.textObject.x;
+    var marginSpace = 2 * this._textObject.x;
     var textAllowedSpace = canvasWidth - marginSpace;
 
     var textWords = text.split(" ");
@@ -212,7 +207,7 @@ nsn.TextManager = function(){
       word = textWords[i];
       possiblePhrase = line + word + " ";
 
-      if(canvasContext.measureText(possiblePhrase).width < textAllowedSpace){
+      if(this.canvasContext.measureText(possiblePhrase).width < textAllowedSpace){
         line += word + " ";
       }else{
         //Decrease index because the word of this iteration wasn't added to this line
@@ -226,10 +221,18 @@ nsn.TextManager = function(){
     textBrokenInLines.push(line);
 
     return textBrokenInLines;
-  };
+  },
 
-  init();
+  _createTextObject: function(){
+    var _textObject = new createjs.Text("", this._font, "#FFFFFF");
+    _textObject.x = 100;
+    _textObject.y = 545;
+    _textObject.textBaseline = "alphabetic";
+    _textObject.lineHeight = 30;
+    _textObject.shadow = new createjs.Shadow("#000000", 2, 2, 0);
 
-  return self;
-
+    return _textObject;
+  }
 };
+
+nsn.TextManager.prototype.constructor = nsn.TextManager;
