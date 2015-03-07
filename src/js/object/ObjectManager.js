4fc13cbc0f52fdfa6826b18ebf2700e57cc06f40
lmatiolis <matioli.leandro@gmail.com>
2015-03-07 12:09:02 -0300

    Refactor the way filters were applied in selected objects.

--- src/js/object/ObjectManager.js
+++ src/js/object/ObjectManager.js
@@ -111,22 +111,35 @@ nsn.ObjectManager.prototype = {
 
   selectObject: function(object){
     this.selectedObject = object;
-    coordinates = this._targetCoordinates(object);
-    x = coordinates.x;
-    y = coordinates.y;
+    this._applyFiltersInSelectedObject(object, [this.colorFilter]);
 
-    /*  Color filter  */
-    this.selectedObject.filters = [this.colorFilter];
-    this.selectedObject.cache(0, 0, object.image.width, object.image.height);
+    /* TODO Check if this is necessary
+     *
+     * Blur e Color Filter Example
+     * Example to show how it needs to be called to mimic the old behavior below
+     *
+     * this.selectedObject.filters = [this.blurFilter, this.colorFilter];
+     * this.selectedObject.cache(this.bounds.x, this.bounds.y, object.image.width + this.bounds.width, object.image.height + this.bounds.height);
+     *
+     * We could simplificate the function _applyFiltersInSelectedObject if the extra parameters were not necessary...
+     */
+     //this._applyFiltersInSelectedObject(object, [this.blurFilter, this.colorFilter], this.bounds.x, this.bounds.y);
+  },
 
-    /*  Blur e Color Filter  */
-    // this.selectedObject.filters = [this.blurFilter, this.colorFilter];
-    // this.selectedObject.cache(this.bounds.x, this.bounds.y, object.image.width + this.bounds.width, object.image.height + this.bounds.height);
+  _applyFiltersInSelectedObject: function(object, filters, cord_x, cord_y){
+    var x = cord_x | 0;
+    var y = cord_y | 0;
 
-    /*  Threshold filter  */
-    // this.selectedObject.filters = [this.brightnessFilter, this.thresholdFilter];
-    // this.selectedObject.cache(0, 0, object.image.width, object.image.height);
+    var width_offset = 0;
+    var height_offset = 0;
 
+    if(cord_x && cord_y){
+      width_offset = this.bounds.width;
+      height_offset = this.bounds.height;
+    }
+
+    this.selectedObject.filters = filters;
+    this.selectedObject.cache(x, y, object.image.width + width_offset, object.image.height + height_offset);
   },
 
   unselectObject: function(){
